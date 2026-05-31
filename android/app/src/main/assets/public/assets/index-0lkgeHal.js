42516804d0a16b2f75f5e1e7e5610ff39461c224
markulsmeh39-source <markulsmeh39@gmail.com>
2026-05-31 13:29:58 +0200

    chore(android): enable cleartext traffic and update config
    Enable cleartext traffic for both the main application and Cordova plugins to support non-HTTPS assets. Additionally, update the Capacitor configuration and push notification settings.

---
+++ android/app/src/main/assets/public/assets/index-0lkgeHal.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DayAKPho.js","assets/index-DvsvCWHW.js","assets/index-DvZNKcaj.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-DvsvCWHW.js";const _=e("Browser",{web:()=>o(()=>import("./web-DayAKPho.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
---
+++ android/app/src/main/assets/public/assets/index-CfDv9Iob.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CGz8akPf.js","assets/index-DvsvCWHW.js","assets/index-DvZNKcaj.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DvsvCWHW.js";const _=p("App",{web:()=>r(()=>import("./web-CGz8akPf.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
