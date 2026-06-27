ec04395aa8aa4032e9ba0dc192b433cac4e7017c
markulsmeh39-source <markulsmeh39@gmail.com>
2026-06-27 17:24:29 +0200

    build(android): add filesystem plugin and patch-package
    - Install @capacitor/filesystem to support file system operations.
    - Integrate patch-package to manage local dependency fixes.
    - Update asset references and build configurations.

---
+++ android/app/src/main/assets/public/assets/index-GwAzJ-UF.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DO4EBr3k.js","assets/index-FL3cO-Be.js","assets/index-C3h7Caw3.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-FL3cO-Be.js";const _=e("Browser",{web:()=>o(()=>import("./web-DO4EBr3k.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
